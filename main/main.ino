64bee969838c2461482470c46f47fa3169aa903f
User1 <user1@yuza1noacBookea.lan>
2019-06-30 20:08:23 +0900

    2019/6/30 でやれたとこまで

--- main/main.ino
+++ main/main.ino
@@ -1,11 +1,41 @@
 #include "env.h"
-#include "camera_pins.h"
 #include <BLEServer.h>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
+#include <OneButton.h>
+// #include <Adafruit_BME280.h>
 
-#define CAMERA_MODEL_TTGO
+#define TTGO_T_CAMERA_V05
+#include "camera_pins.h"
+#include "esp_camera.h"
+
+/***************************************
+ *  Modules
+ **************************************/
+#define ENABLE_SSD1306
+// #define ENABLE_BME280
+#define ENABLE_SLEEP
+#define ENABLE_AS312
+#define ENABLE_BUTTON
+
+#ifdef ENABLE_SSD1306
+#include "SSD1306.h"
+#include "OLEDDisplayUi.h"
+#define SSD1306_ADDRESS 0x3c
+SSD1306 oled(SSD1306_ADDRESS, I2C_SDA, I2C_SCL, SSD130_MODLE_TYPE);
+OLEDDisplayUi ui(&oled);
+#endif
+
+#ifdef ENABLE_BUTTON
+OneButton button1(BUTTON_1, true);
+#endif
+
+#ifdef ENABLE_BME280
+#define BEM280_ADDRESS 0X77
+#define SEALEVELPRESSURE_HPA (1013.25)
+Adafruit_BME280 bme;
+#endif
 
 // Device Name: Maximum 30 bytes
 #define DEVICE_NAME "LINE Things Trial ESP32"
@@ -20,9 +50,6 @@
 #define PSDI_SERVICE_UUID "E625601E-9E55-4597-A598-76018A0D293D"
 #define PSDI_CHARACTERISTIC_UUID "26E2B12B-85F0-4F3F-9FDD-91D114270E6E"
 
-#define BUTTON 0
-#define LED1 2
-
 BLEServer* thingsServer;
 BLESecurity *thingsSecurity;
 BLEService* userService;
@@ -35,6 +62,7 @@ bool deviceConnected = false;
 bool oldDeviceConnected = false;
 
 volatile int btnAction = 0;
+uint8_t btnValue;
 
 class serverCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -49,19 +77,73 @@ class serverCallbacks: public BLEServerCallbacks {
 class writeCallback: public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *bleWriteCharacteristic) {
     std::string value = bleWriteCharacteristic->getValue();
-    if ((char)value[0] <= 1) {
-      digitalWrite(LED1, (char)value[0]);
-    }
+    Serial.println(value.c_str());
+    oled.setFont(ArialMT_Plain_16);
+    oled.setTextAlignment(TEXT_ALIGN_LEFT);
+    delay(50);
+    oled.drawString(10, 16, value.c_str());
+    oled.display();
+    delay(50);
   }
 };
 
+void buttonClick()
+{
+  btnValue = 1;
+  notifyCharacteristic->setValue(&btnValue, 1);
+  notifyCharacteristic->notify();
+  oled.setFont(ArialMT_Plain_16);
+  oled.setTextAlignment(TEXT_ALIGN_LEFT);
+  oled.clear();
+  delay(50);
+  oled.drawString(5, 5, "Hello LINE Things");
+  oled.display();
+  delay(50);
+}
+
+void buttonLongPress()
+{
+  btnValue = 0;
+  notifyCharacteristic->setValue(&btnValue, 1);
+  notifyCharacteristic->notify();
+  oled.setFont(ArialMT_Plain_16);
+  oled.setTextAlignment(TEXT_ALIGN_LEFT);
+  oled.clear();
+  delay(50);
+  oled.drawString(5, 40, "Released");
+  oled.display();
+  delay(50);
+}
+
+#ifdef ENABLE_SSD1306
+void drawFrame1(OLEDDisplay *display, OLEDDisplayUiState *state, int16_t x, int16_t y)
+{
+    display->setTextAlignment(TEXT_ALIGN_CENTER);
+
+    int y1 = 25;
+
+    if (oled.getHeight() == 32) {
+        y1 = 10;
+    }
+
+    display->setFont(ArialMT_Plain_16);
+    display->drawString(64 + x, 5 + y, "Hello LINE Things");
+}
+
+FrameCallback frames[] = {drawFrame1};
+#define FRAMES_SIZE (sizeof(frames) / sizeof(frames[0]))
+#endif
+
 void setup() {
   Serial.begin(115200);
+  Serial.setDebugOutput(true);
 
-  pinMode(LED1, OUTPUT);
-  digitalWrite(LED1, 0);
-  pinMode(BUTTON, INPUT_PULLUP);
-  attachInterrupt(BUTTON, buttonAction, CHANGE);
+  //pinMode(LED1, OUTPUT);
+  //digitalWrite(LED1, 0);
+  //pinMode(BUTTON, INPUT_PULLUP);
+  //attachInterrupt(BUTTON, buttonAction, CHANGE);
+  button1.attachClick(buttonClick);
+  button1.attachLongPressStart(buttonLongPress);
 
   BLEDevice::init("");
   BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_NO_MITM);
@@ -74,19 +156,79 @@ void setup() {
 
   setupServices();
   startAdvertising();
+
+/*
+  Serial.print("Camera Settings...");
+
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  //init with high specs to pre-allocate larger buffers
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+
+  // camera init
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_framesize(s, FRAMESIZE_QVGA);
+
+  Serial.print("Camera Ready!");
+*/
+
+  int x = oled.getWidth() / 2;
+  int y = oled.getHeight() / 2;
+  oled.init();
+  Wire.setClock(100000);  //! Reduce the speed and prevent the speed from being too high, causing the screen
+  oled.setFont(ArialMT_Plain_16);
+  oled.setTextAlignment(TEXT_ALIGN_CENTER);
+  delay(50);
+  oled.drawString(x, y + 10, "Ready to Connect");
+  oled.display();
+
+#ifdef ENABLE_SSD1306
+  ui.setTargetFPS(30);
+  ui.setIndicatorPosition(BOTTOM);
+  ui.setIndicatorDirection(LEFT_RIGHT);
+  ui.setFrameAnimation(SLIDE_LEFT);
+  ui.setFrames(frames, FRAMES_SIZE);
+  ui.setTimePerFrame(6000);
+#endif
+  
   Serial.println("Ready to Connect");
 }
 
 void loop() {
-  uint8_t btnValue;
-
-  while (btnAction > 0 && deviceConnected) {
-    btnValue = !digitalRead(BUTTON);
-    btnAction = 0;
-    notifyCharacteristic->setValue(&btnValue, 1);
-    notifyCharacteristic->notify();
-    delay(20);
-  }
+  
   // Disconnection
   if (!deviceConnected && oldDeviceConnected) {
     delay(500); // Wait for BLE Stack to be ready
@@ -97,6 +239,8 @@ void loop() {
   if (deviceConnected && !oldDeviceConnected) {
     oldDeviceConnected = deviceConnected;
   }
+
+  button1.tick();
 }
 
 void setupServices(void) {
@@ -141,8 +285,4 @@ void startAdvertising(void) {
   thingsServer->getAdvertising()->addServiceUUID(userService->getUUID());
   thingsServer->getAdvertising()->setScanResponseData(scanResponseData);
   thingsServer->getAdvertising()->start();
-}
-
-void buttonAction() {
-  btnAction++;
 }
